62cfd73c27f4c4bf3e7032b187c80f7426228805
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:45:32 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+     //echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';       Il faut traduire ça
+    $age = 18;
+    if ($age >= 18) {
+      echo 'Tu es majeur';
+    }
+    else {
+      echo 'Tu es mineur';
+    }
+    ?>
+
+  </body>
+</html>
